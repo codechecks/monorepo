76f225e2e2a8fe6bc7d87ee32451dab3916f891f
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-07-06 12:55:01 +0200

    switch to jest, remove mocha, remove unneeded dependencies

--- packages/client/src/ci-providers/__tests__/Circle.test.ts
+++ packages/client/src/ci-providers/__tests__/Circle.test.ts
@@ -1,5 +1,4 @@
 import { join } from "path";
-import { expect } from "chai";
 
 import { readEnvFile } from "./utils";
 import { Circle } from "../Circle";
@@ -11,43 +10,43 @@ describe("Circle", () => {
   it("should detect circleci", () => {
     const provider = new Circle(env);
 
-    expect(provider.isCurrentlyRunning()).to.be.true;
+    expect(provider.isCurrentlyRunning()).toBe(true);
   });
 
   it("should not detect circleci when not running inside circleci", () => {
     const provider = new Circle({});
 
-    expect(provider.isCurrentlyRunning()).to.be.false;
+    expect(provider.isCurrentlyRunning()).toBe(false);
   });
 
   it("should get pull request id", () => {
     const provider = new Circle(env);
 
-    expect(provider.getPullRequestID()).to.be.eq(10);
+    expect(provider.getPullRequestID()).toBe(10);
   });
 
   it("should not get pull request id if not running in PR context", () => {
     const env = readEnvFile(join(__dirname, "__fixtures__", ".circleci-nopr.env"));
     const provider = new Circle(env);
 
-    expect(provider.getPullRequestID()).to.be.undefined;
+    expect(provider.getPullRequestID()).toBeUndefined();
   });
 
   it("should get target SHA", () => {
     const provider = new Circle(env);
 
-    expect(provider.getCurrentSha()).to.be.eq("711a2ba0e24c74b5e818dc0b130912db61e804fe");
+    expect(provider.getCurrentSha()).toBe("711a2ba0e24c74b5e818dc0b130912db61e804fe");
   });
 
   it("should not detect fork mode", () => {
     const provider = new Circle(env);
 
-    expect(provider.isFork()).to.be.eq(false);
+    expect(provider.isFork()).toBe(false);
   });
 
   it("should detect fork mode", () => {
     const provider = new Circle(envFork);
 
-    expect(provider.isFork()).to.be.eq(true);
+    expect(provider.isFork()).toBe(true);
   });
 });
