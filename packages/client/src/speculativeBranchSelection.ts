d7acebd038931d93b7a09e27f377fc6a28284fe7
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-07-13 13:57:12 +0200

    remove dependency on nodegit

--- packages/client/src/speculativeBranchSelection.ts
+++ packages/client/src/speculativeBranchSelection.ts
@@ -1,28 +1,27 @@
-import * as Git from "nodegit";
-
 import { PrInfo, FileStatuses } from "./api";
 import { CodeChecksSettings } from "./types";
 import { logger } from "./logger";
 import execa = require("execa");
+
+const diffParser = require("./diff-parser/diff-parser.js").DiffParser;
 
 export async function getPrInfoForSpeculativeBranch(
   settings: CodeChecksSettings,
   gitRepoRootPath: string,
 ): Promise<PrInfo | undefined> {
   logger.debug("Trying speculative branch execution");
-  const repo = await Git.Repository.open(gitRepoRootPath);
-  const headCommit = await repo.getHeadCommit();
-  const baseCommit = await getBaseCommit(repo, settings.branches);
+  const headCommit = await getHeadCommit(gitRepoRootPath);
+  const baseCommit = await getBaseCommit(gitRepoRootPath, settings.branches);
   if (!baseCommit) {
     return;
   }
-  if (baseCommit.sha() === headCommit.sha()) {
+  if (baseCommit === headCommit) {
     throw new Error(
-      "Speculative branch selection failed. baseCommit can't be equal to headCommit. Please create Pull Request to skip this problem.",
+      `Speculative branch selection failed. baseCommit can't be equal to headCommit (${baseCommit}). Please create Pull Request to skip this problem.`,
     );
   }
 
-  const fileStatuses = await getFileStatuses(repo, baseCommit, headCommit);
+  const fileStatuses = await getFileStatuses(gitRepoRootPath, baseCommit, headCommit);
 
   return {
     id: 0,
@@ -31,34 +30,36 @@ export async function getPrInfoForSpeculativeBranch(
       body: "",
     },
     head: {
-      sha: headCommit.sha(),
+      sha: headCommit,
     },
     base: {
-      sha: baseCommit.sha(),
+      sha: baseCommit,
     },
     files: fileStatuses,
   };
 }
 
-async function getBaseCommit(
-  repo: Git.Repository,
-  speculativeBranchesInOrder: string[],
-): Promise<Git.Commit | undefined> {
-  const headBranch = await repo.getCurrentBranch();
-  const baseBranchName = findSpeculativeBaseBranch(headBranch.shorthand(), speculativeBranchesInOrder);
+async function getHeadCommit(repoPath: string): Promise<string> {
+  return await run(repoPath, "git rev-parse HEAD");
+}
+
+async function getBaseCommit(repoPath: string, speculativeBranchesInOrder: string[]): Promise<string | undefined> {
+  const headBranch = await run(repoPath, `git rev-parse --abbrev-ref HEAD`);
+  const baseBranchName = findSpeculativeBaseBranch(headBranch, speculativeBranchesInOrder);
 
   logger.debug({ baseBranchName });
 
   if (baseBranchName) {
     // @NOTE: this might be CircleCI specific thing but for some reason we NEED to take remote branch origin/* because local one is somehow already updated but this might now work on some environments...
     try {
-      const { stdout, stderr } = await execa("git fetch --all", { shell: true });
-      logger.debug({ stdout, stderr });
-      return await repo.getBranchCommit(`origin/${baseBranchName}`);
+      await run(repoPath, `git fetch origin ${baseBranchName}`);
+      const sha = await run(repoPath, `git rev-parse FETCH_HEAD`);
+      return sha;
     } catch (e) {
       logger.debug(e);
       logger.debug(`Failed to access origin/${baseBranchName}. Trying with: ${baseBranchName}`);
-      return await repo.getBranchCommit(baseBranchName);
+      const { stdout: sha } = await execa(`git rev-parse ${baseBranchName}`, { shell: true, cwd: repoPath });
+      return sha;
     }
   }
 }
@@ -75,28 +76,25 @@ function findSpeculativeBaseBranch(
   return speculativeBranchesInOrder[currentBranchInOrder + 1];
 }
 
-async function getFileStatuses(
-  repo: Git.Repository,
-  baseCommit: Git.Commit,
-  headCommit: Git.Commit,
-): Promise<FileStatuses> {
-  const diff = await Git.Diff.treeToTree(repo, await baseCommit.getTree(), await headCommit.getTree());
-  const patches = await diff.patches();
+async function getFileStatuses(repo: string, baseCommit: string, headCommit: string): Promise<FileStatuses> {
+  const diffRaw = await run(repo, `git diff ${baseCommit}..${headCommit}`);
+  const diffs = diffParser.generateDiffJson(diffRaw);
 
   const added: string[] = [];
   const changed: string[] = [];
   const removed: string[] = [];
 
   // todo fix paths
-  for (const patch of patches) {
-    if (patch.isAdded()) {
-      added.push(patch.newFile().path());
-    } else if (patch.isModified()) {
-      changed.push(patch.newFile().path());
-    } else if (patch.isDeleted()) {
-      removed.push(patch.oldFile().path());
+  for (const diff of diffs) {
+    if (diff.isNew) {
+      added.push(diff.newName);
+    } else if (diff.isDeleted) {
+      removed.push(diff.oldName);
+    } else if (diff.isRename) {
+      removed.push(diff.oldName);
+      added.push(diff.newName);
     } else {
-      throw new Error(`Unrecognized changed status. ${patch}`);
+      changed.push(diff.newName);
     }
   }
 
@@ -105,4 +103,10 @@ async function getFileStatuses(
     added,
     removed,
   };
+}
+
+export async function run(cwd: string, cmd: string): Promise<string> {
+  const { stdout } = await execa(cmd, { shell: true, cwd });
+
+  return stdout;
 }
