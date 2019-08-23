dc48ce91f07e1125038fe9b371490427d7960648
Craig Thompson <craig.thompson@hive.hr>
2019-08-23 13:38:37 +0100

    Create Semaphore2.ts

---
+++ packages/client/src/ci-providers/Semaphore2.ts
@@ -0,0 +1,47 @@
+import { Env, CiProvider } from "./types";
+
+export class Semaphore implements CiProvider {
+  constructor(private readonly env: Env) {}
+
+  isCurrentlyRunning(): boolean {
+    return this.env["SEMAPHORE"] === "true";
+  }
+
+  getPullRequestID(): number | undefined {
+    if (!this.env["SEMAPHORE_GIT_BRANCH"]) {
+      return undefined;
+    }
+
+    return this.env.SEMAPHORE_GIT_BRANCH.substr(13);
+  }
+
+  getCurrentSha(): string {
+    const sha = this.env["SEMAPHORE_GIT_SHA"];
+    if (!sha) {
+      throw new Error("Couldnt get target SHA");
+    }
+
+    return sha;
+  }
+
+  isFork(): boolean {
+    if (!this.env["SEMAPHORE_GIT_BRANCH"]) {
+      return false;
+    }
+
+    return this.env.SEMAPHORE_GIT_BRANCH.startsWith("pull-request-");
+  }
+
+  public getProjectSlug(): string {
+    const slug = this.env["SEMAPHORE_GIT_REPO_SLUG"];
+    if (!slug) {
+      throw new Error("Couldnt get repo slug");
+    }
+
+    return slug;
+  }
+
+  public supportsSpeculativeBranchSelection(): boolean {
+    return true;
+  }
+}
