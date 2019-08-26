c30867648887f9d6591391287cd633093e9a965b
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-25 21:29:25 -0400

    fixup

--- checks/semaphore.ts
+++ checks/semaphore.ts
@@ -4,6 +4,6 @@ export default async function() {
   await codechecks.success({
     name: "Semaphore",
     shortDescription: "works!",
-    longDescription: "```" + JSON.stringify(codechecks.context, null, 2) + "```\n",
+    longDescription: "```\n" + JSON.stringify(codechecks.context, null, 2) + "```\n",
   });
 }
