ec8b7b64dd769ee26c406118c6ab43ee58c00f7c
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-09-07 15:55:44 -0700

    improve audit script

--- .circleci/audit-client-package.sh
+++ .circleci/audit-client-package.sh
@@ -5,13 +5,17 @@ cd "$(dirname "$0")"
 # this could be a separate, reusable check in futue
 
 cd ../packages/client
-yarn link
+echo "Packing package"
+npm pack
+mv codechecks-client-*.tgz /tmp/codechecks-client.tgz
 
+echo "Creating dummy client package and reinstalling client"
+rm -rf /tmp/codechecks-client-audit
 mkdir -p /tmp/codechecks-client-audit
 cd /tmp/codechecks-client-audit
 
 yarn init -y 
-yarn add @codechecks/client
+yarn add /tmp/codechecks-client.tgz
 
 echo "Conducting audit..."
 yarn audit
