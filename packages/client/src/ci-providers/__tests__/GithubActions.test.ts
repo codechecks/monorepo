b3f7953788adac48f14f9779b6da9255064c177c
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-05-06 00:59:31 +0200

    Fix support for GHA

--- packages/client/src/ci-providers/__tests__/GithubActions.test.ts
+++ packages/client/src/ci-providers/__tests__/GithubActions.test.ts
@@ -4,42 +4,51 @@ import { readEnvFile } from "./utils";
 import { GithubActions } from "../GithubActions";
 
 describe("Github Actions", () => {
-  const env = readEnvFile(join(__dirname, "__fixtures__", "githubActions/pr.env"));
-  const envFork = readEnvFile(join(__dirname, "__fixtures__", "githubActions/fork.env"));
+  describe("PR", () => {
+    const env = readEnvFile(join(__dirname, "__fixtures__", "githubActions/pr.env"));
 
-  it("should detect github", () => {
-    const provider = new GithubActions(env);
+    it("works", () => {
+      const provider = new GithubActions(env);
 
-    expect(provider.isCurrentlyRunning()).toBe(true);
+      expect(provider.isCurrentlyRunning()).toBe(true);
+      expect(provider.isFork()).toBe(false);
+      expect(provider.getPullRequestID()).toBe(4);
+      expect(provider.getProjectSlug()).toBe("test12345678t/actions-tests");
+      expect(provider.getCurrentSha()).toBe("e4ff1a0b045b110a613ae7dfcd324546645c8571");
+    });
+  });
+
+  describe("Not PR", () => {
+    const env = readEnvFile(join(__dirname, "__fixtures__", "githubActions/nopr.env"));
+
+    it("works", () => {
+      const provider = new GithubActions(env);
+
+      expect(provider.isCurrentlyRunning()).toBe(true);
+      expect(provider.isFork()).toBe(false);
+      expect(provider.getPullRequestID()).toBe(undefined);
+      expect(provider.getProjectSlug()).toBe("test12345678t/actions-tests");
+      expect(provider.getCurrentSha()).toBe("3941c1db650b68763a4a3b66c049bddc2ae9aad3");
+    });
+  });
+
+  describe("fork", () => {
+    const env = readEnvFile(join(__dirname, "__fixtures__", "githubActions/fork.env"));
+
+    it("works", () => {
+      const provider = new GithubActions(env);
+
+      expect(provider.isCurrentlyRunning()).toBe(true);
+      expect(provider.isFork()).toBe(true);
+      expect(provider.getProjectSlug()).toBe("krzkaczor/actions-tests");
+      expect(provider.getPullRequestID()).toBe(3);
+      expect(provider.getCurrentSha()).toBe("6a0c7cd10b59d2520ea8a5f4860aa98f167cb27a");
+    });
   });
 
   it("should not detect github when not running inside github", () => {
     const provider = new GithubActions({});
 
     expect(provider.isCurrentlyRunning()).toBe(false);
-  });
-
-  it("should not get pull request id", () => {
-    const provider = new GithubActions(env);
-
-    expect(provider.getPullRequestID()).toBe(undefined);
-  });
-
-  it("should get target SHA", () => {
-    const provider = new GithubActions(env);
-
-    expect(provider.getCurrentSha()).toBe("ffac537e6cbbf934b08745a378932722df287a53");
-  });
-
-  it("should not detect fork mode", () => {
-    const provider = new GithubActions(env);
-
-    expect(provider.isFork()).toBe(false);
-  });
-
-  it("should detect fork mode", () => {
-    const provider = new GithubActions(envFork);
-
-    expect(provider.isFork()).toBe(true);
   });
 });
